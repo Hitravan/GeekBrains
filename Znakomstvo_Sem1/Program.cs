f0265c13ea2439b31bd9c16a92aa9095974aa5d5
Andrey <andreikuznecov94@gmail.com>
2023-11-21 14:03:09 +0300

    Переменные и операции с ними

---
+++ Znakomstvo_Sem1/Program.cs
@@ -0,0 +1,32 @@
+﻿// int - целое число
+// string - строка (текст)
+// double (float) - число с плавающей точкой
+// bool - true/false
+
+//Условия if else и else if
+int number = 5;
+
+if (number < 6)
+{
+    Console.WriteLine(number);
+}
+else if ()
+{
+
+}
+else
+{
+    Console.WriteLine("Нет");
+}
+
+//Операции с целыми и дробными числами
+int number2 = 6;
+double number3 = 4.5;
+Console.WriteLine(number2 * number3);
+
+//Склеивание строк
+string name = "Andrey";
+string age = "29";
+Console.WriteLine(name + " " + age);
+
+
