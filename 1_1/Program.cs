9cdf4073ab370d0f5173c0f7b7953b3cb7d5a012
Andrey <andreikuznecov94@gmail.com>
2023-12-07 09:10:40 +0300

    возведение в квадрат

---
+++ 1_1/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Write(" Введите число ");
+string input = Console.ReadLine();
+int number = Convert.ToInt32(input);
+
+int sqrtNumber = number * number;
+
+Console.Write(" Число " + number + " в квадрате равно ");
+Console.WriteLine(sqrtNumber);
