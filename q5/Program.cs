a544c53212aad5765f9f53e8c047a5ee247b8d57
Andrey <andreikuznecov94@gmail.com>
2023-12-07 10:45:50 +0300

    промежуток от -N до N

---
+++ q5/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите число");
+int input = Convert.ToInt32(Console.ReadLine());
+int mininput = -input;
+
+while (input >= mininput)
+{
+    Console.Write(mininput + " ");
+
+    mininput += 1;
+}
