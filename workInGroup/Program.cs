a73e580623dc42c2ffd3dfd16a9b3a8c48c25ff0
Andrey <andreikuznecov94@gmail.com>
2023-12-07 09:55:39 +0300

    является ли число а корнем числа b

---
+++ workInGroup/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Write(" Введите число a ");
+string inputA = Console.ReadLine();
+int numberA = Convert.ToInt32(inputA);
+
+Console.Write(" Введите число b ");
+string inputB = Console.ReadLine();
+int numberB = Convert.ToInt32(inputB);
+
+int sqrtNumber = numberB * numberB;
+
+if (sqrtNumber == numberA)
+{
+    Console.WriteLine(" Число " + inputA + " является корнем " + inputB);
+}
+else
+{
+    Console.WriteLine(" Число " + inputA + " не является корнем " + inputB);
+}
